8a9be48d6dca907c401b84779ea6c9f7ee0700fc
Kyle Nakamura <kjnakamura@apu.edu>
2020-10-30 17:29:30 -0700

    Refactor (simplify) UseKey

--- src/components/UseKey.js
+++ src/components/UseKey.js
@@ -6,19 +6,16 @@ function useKey(key) {
     const match = event => key.toLowerCase() === event.key.toLowerCase()
 
     const onDown = event => {
-        if (match(event)) setPressed(true);
-    }
-
-    const onUp = event => {
-        if (match(event)) setPressed(false);
+        if (match(event)) {
+            setPressed(true);
+            setPressed(false);
+        }
     }
 
     useEffect(() => {
         window.addEventListener("keydown", onDown)
-        window.addEventListener("keyup", onUp)
         return () => {
             window.removeEventListener("keydown", onDown)
-            window.removeEventListener("keyup", onUp)
         }
     }, [key])
 
